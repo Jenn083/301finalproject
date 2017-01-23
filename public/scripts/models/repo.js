12b079fc6b8eb1d6e16a8f44d00e2f0304bfd27f
Shelly Tang <shellytang@gmail.com>
2017-01-23 13:38:59 -0800

    initial work on ajax

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -7,10 +7,22 @@
 
   repos.requestRepos = function(callback) {
     // DONE: Refactor your ajax call to use the $.get method, and make a request to our new proxy route.
-    $.get('/github/user/repos?per_page=5&sort=updated') //remove .com!!
+    $.get('kingcounty/resource/gkhn-e8mn.json?zip_code=98109') //remove .com!!
     .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
     .then(callback);
   };
+
+  $.ajax({
+    url: 'https://data.kingcounty.gov/resource/f29f-zza5.json?zip_code=98109',
+    type: 'GET',
+    data: {
+      '$limit' : 5000,
+      '$$app_token': '9vDySgx7Ckb1nzJ38UuD02Hbw'
+    }
+  }).done(function(data) {
+  // alert("Retrieved " + data.length + " records from the dataset!");
+    console.log(data);
+  });
 
   repos.with = attr => repos.all.filter(repo => repo[attr]);
 
