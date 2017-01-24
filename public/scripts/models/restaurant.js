628d35f3a076357f5e71d5dc0abb5cb3a7cfead2
jenn083 <nguyenj1203@gmail.com>
2017-01-23 17:35:34 -0800

    created instances

--- public/scripts/models/restaurant.js
+++ public/scripts/models/restaurant.js
@@ -20,7 +20,7 @@
   restaurants.requestRestaurant = function(callback) {
     // DONE: Refactor your ajax call to use the $.get method, and make a request to our new proxy route.
     $.get('https://data.kingcounty.gov/resource/gkhn-e8mn.json?$$app_token=9vDySgx7Ckb1nzJ38UuD02Hbw&$where=inspection_date%20between%20%272015-01-10T12:00:00%27%20and%20%272017-01-10T14:00:00%27&$limit=2000') //remove .com!!
-    .then(data => restaurants.all = data, err => console.error(err)) // es6 syntax arrow functions
+    .then(data => data.forEach(restaurant => { new Restaurant(restaurant); })) // es6 syntax arrow functions
     .then(callback);
   };
 
