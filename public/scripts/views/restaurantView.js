d212379aa1c20c2ef8374c5f6302ecc59d1960ac
alisonramer <alisonramer@gmail.com>
2017-01-25 14:39:07 -0800

    maps.event

--- public/scripts/views/restaurantView.js
+++ public/scripts/views/restaurantView.js
@@ -32,7 +32,12 @@
       };
       console.log(markerOption);
       var marker = new google.maps.Marker(markerOption);
+
       // restaurantView.markers.push(marker);
+      google.maps.event.addListener(marker, 'click', function(){
+        console.log ('does this work?');
+      })
+
     });
   }
   module.restaurantView = restaurantView;
