ce11d6c3df1eb7362ada7e57cc0a7d959a6c8f30
alisonramer <alisonramer@gmail.com>
2017-01-25 15:10:26 -0800

    test4  of appending

--- public/scripts/views/restaurantView.js
+++ public/scripts/views/restaurantView.js
@@ -35,7 +35,7 @@
 
       // restaurantView.markers.push(marker);
       google.maps.event.addListener(marker, 'click', function(){
-        document.getElementById("displayMarkerOptions").innerHTML = this.restaurants.all;
+        document.getElementById("displayMarkerOptions").innerHTML = this.restaurants;
 
         console.log ('does this work?');
       })
