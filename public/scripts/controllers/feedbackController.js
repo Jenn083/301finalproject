3fd9f16a3a7f124079f659d4b95845b103b8d2aa
jenn083 <nguyenj1203@gmail.com>
2017-01-26 06:35:15 -0800

    modified db

--- public/scripts/controllers/feedbackController.js
+++ public/scripts/controllers/feedbackController.js
@@ -5,23 +5,47 @@
 
   feedbackController.index = () => {
     $('#feedback').show().siblings().hide();
+    loadFeedback();
     $('#button').on('click', send);
   };
+
+  var loadFeedback = () => {
+    // load feedback from database
+    $.get('/feedback/all')
+     .then( (feedback) => {
+       $('#guestbook').empty();
+       feedback.rows.map( (item) => {
+         var comment = `
+           <div class="comment">
+             <h2 class="sender">${item.author}</h2>
+             <p class="message">${item.message}</p>
+           </div>
+         `;
+         $('#guestbook').append(comment);
+       });
+     });
+  }
 
   var send = (event) => {
     event.preventDefault();
     var name = $('#nameSender').val();
-    var feedback = $('#feedbackContent').val();
+    var message = $('#feedbackContent').val();
 
     var comment = `
       <div class="comment">
         <h2 class="sender">${name}</h2>
-        <p class="message">${feedback}</p>
+        <p class="message">${message}</p>
       </div>
     `;
     $('#guestbook').append(comment);
-    $('#nameSender').val('');
-    $('#feedbackContent').val('')
+    $('#nameSender').val('');//reset val to empty string
+    $('#feedbackContent').val('');
+    var feedback = {
+      author: name,
+      message: message
+    };
+    // save to database
+    $.post('/feedback/insert', feedback);
   }
 
 
